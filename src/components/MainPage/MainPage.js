0583d92d56d6b11c0fda981a009778a0c4b232e4
ksmani <kumar.mani017@gmail.com>
2020-08-13 17:27:25 +0530

    Card Addition

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import { Container, Grid, AppBar, Toolbar, TextField, Typography } from '@material-ui/core';
+import { Container, Grid, AppBar, Toolbar, TextField, Typography, Card, CardContent } from '@material-ui/core';
 
-const MainPage = ()=>{
+const MainPage = () => {
     return (
         <>
-        <AppBar position="static" style={{marginBottom: 20}}>
-            <Toolbar>
-                <Typography variant="h6">
-                    FTL Assignment | Mani
+            <AppBar position="static" style={{ marginBottom: 20 }}>
+                <Toolbar>
+                    <Typography variant="h6">
+                        FTL Assignment | Mani
                 </Typography>
-            </Toolbar>
-        </AppBar>
-        <Container maxWidth="sm">
-            <Grid container direction="column" alignItems="center">
-                <Grid item style={{width: '100%', marginBottom: 50}}>
-                    <TextField
-                        fullWidth 
-                        variant="outlined" 
-                        placeholder="Type User Name"/>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="sm">
+                <Grid container direction="column" alignItems="center">
+                    <Grid item style={{ width: '100%', marginBottom: 50 }}>
+                        <TextField
+                            fullWidth
+                            variant="outlined"
+                            placeholder="Type User Name" />
+                    </Grid>
+                    <Grid item container>
+                        <Card style={{ width: '100%' }}>
+                            <CardContent>
+                                <Grid container justify="space-between">
+                                    <p>Name : mani</p>
+                                    <p>TimeZone : kumaar</p>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <p>sdfbhwokm</p>
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
         </>
     )
 }
