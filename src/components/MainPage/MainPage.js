5944a46bbe02ba4d361ae754d50aacbae41cd502
ksmani <kumar.mani017@gmail.com>
2020-08-13 21:34:41 +0530

    Card Content Addition

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grid, AppBar, Toolbar, TextField, Typography, Card, CardContent } from '@material-ui/core';
+import { Container, Grid, AppBar, Toolbar, TextField, Typography, Card, CardContent, Box } from '@material-ui/core';
 
 import userAxios from '../../userAxios'
 
 const MainPage = () => {
     const [user, setUser] = useState([]);
+    const [textBoxValue, setTextBoxValue] = useState('');
+
+    const textBoxHandler = (event) => {
+        event.preventDefault();
+        setTextBoxValue(event.target.value)
+    }
 
     useEffect(() => {
         userAxios.get("/")
@@ -28,25 +34,53 @@ const MainPage = () => {
                         <TextField
                             fullWidth
                             variant="outlined"
-                            placeholder="Type User Name" />
+                            placeholder="Type User Name"
+                            onChange={textBoxHandler} />
                     </Grid>
                     <Grid item container>
                         {
-                            user.map((item) => (
-                                <Card style={{ width: '100%' }}>
-                                    <CardContent>
-                                        <Grid container justify="space-between">
-                                            <p>Name : {item.real_name}</p>
-                                            <p>TimeZone : {item.tz}</p>
-                                        </Grid>
-                                    </CardContent>
-                                </Card>
-                            ))
+                            textBoxValue===''?
+                                user.map((item) => (
+                                    <CardContainer key={item.id} id={item.id} realName={item.real_name} tz={item.tz}/>
+                                )):
+                                user
+                                    .filter(item=>item.real_name.toLowerCase().includes(textBoxValue.toLowerCase())||item.tz.toLowerCase().includes(textBoxValue.toLowerCase()))
+                                    .map(item=>(
+                                        <CardContainer key={item.id} id={item.id} realName={item.real_name} tz={item.tz}/>
+                                    ))
                         }
                     </Grid>
                 </Grid>
             </Container>
         </>
+    )
+}
+
+
+function CardContainer(props) {
+    return (
+        <Card raised style={{ width: '100%', borderBottom: '4px solid #ec2029', marginBottom: 20 }}>
+            <CardContent>
+                <Grid container justify="space-between">
+                    <Grid item style={{ width: '40%' }}>
+                        <Box component="div">
+                            <Typography variant="caption">Name</Typography>
+                        </Box>
+                        <Box component="div">
+                            <Typography variant="body1">{props.realName}</Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item style={{ width: '40%' }}>
+                        <Box component="div">
+                            <Typography variant="caption">Time Zone</Typography>
+                        </Box>
+                        <Box component="div">
+                            <Typography variant="body1">{props.tz.split("/").join(" / ")}</Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
     )
 }
 
